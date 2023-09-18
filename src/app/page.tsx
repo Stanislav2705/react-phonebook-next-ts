01414b7865dd4e063ad8f6947d1e9ec9a126b024
Stanislav Rohal <stasrogal25@gmail.com>
2023-09-18 22:55:55 +0300

    install packege

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import {Title,Box} from './page.styled'
+
+export default function Home() {
+  return (
+    <Box>
+      <Title>
+        CREATE YOUR<br />OWN PHONEBOOK
+      </Title>
+    </Box>
+  )
+}
